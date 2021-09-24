413d47d7c4960373c37218c01a894ded0e224650
Ekansh Saxena <ekansh005@gmail.com>
2021-09-24 21:41:45 +0300

    Half template done

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#252632",
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+      borderRadius: {
+        big: "3rem",
+      },
+      spacing: {
+        112: "28rem",
+        128: "32rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
